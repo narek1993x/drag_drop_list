0004b8fa0013e183beef99c4020660d1eeab7cc0
Narek Khachatryan <narek1993x@mail.ru>
2018-05-31 00:15:01 +0400

    made app and header components stateless

--- src/app.js
+++ src/app.js
@@ -2,28 +2,25 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { DragDropContext } from 'react-dnd'
 import HTML5Backend from 'react-dnd-html5-backend'
-import SubHeader from './components/SubHeader'
+import Header from './components/Header'
 import InputList from './components/InputList'
 
-@DragDropContext(HTML5Backend)
-class App extends Component {
-  render() {
-    const { prosList, consList } = this.props
-    const inputList = [
-      {key: 'pros', list: prosList },
-      {key: 'cons', list: consList }
-    ]
-    return (
-      <div className='App'>
-        <SubHeader />
-        <div className='Boards'>
-          {inputList.map((item, i) => (
-            <InputList keyList={item.key} id={i} key={i} list={item.list} />
-          ))}
-        </div>
-      </div>  
-    );
-  }
+
+const App = ({prosList, consList}) => {
+  const inputList = [
+    {key: 'pros', list: prosList },
+    {key: 'cons', list: consList }
+  ]
+  return (
+    <div className='App'>
+      <Header />
+      <div className='Content'>
+        {inputList.map((item, i) => (
+          <InputList keyList={item.key} id={i} key={i} list={item.list} />
+        ))}
+      </div>
+    </div>  
+  )
 }
 
 const mapStateToProps = state => ({
@@ -31,4 +28,4 @@ const mapStateToProps = state => ({
   consList: state.prosCons.cons
 })
 
-export default connect(mapStateToProps)(App)
+export default DragDropContext(HTML5Backend)(connect(mapStateToProps)(App))
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+const Header = props => {
+  return (
+    <header className='Header'>
+      <h2>Should I eat at McDonalds?</h2>
+    </header>
+  )
+}
+
+export default Header
